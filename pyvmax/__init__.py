40d7b1d304fbe62d98e47ec8f9dbaef0972bbcf5
scottbri <scott@brightwell.org>
2016-06-10 12:00:06 -0400

    added logging functionality

--- pyvmax/__init__.py
+++ pyvmax/__init__.py
@@ -1,14 +1,21 @@
 __version__ = "0.8.2"
 
 from pyvmax.Restful import Restful
+import pyvmax.Logger 
 
 def connect(url, username, password):
+
+    Logger.logger_setup()
+    log = Logger.get_logger('pyvmax')
+    log.info("beginning initial api connection")
 
     rest_client = Restful(url, username, password)
     target_uri = "%s/univmax/restapi/system/version" % (url)
     version_response = rest_client.get(target_uri)
     if 'version' in version_response:
         univmax_version = version_response.get('version', '')
+    else:
+        log.debug('Incorrect version response')
 
 # import the correct API module for the version of Unisphere discovered
     version_major = ""
@@ -18,12 +25,15 @@ def connect(url, username, password):
         version_major, version_minor = '.'.join(_version_parts[:2]), '.'.join(_version_parts[2:])
     if version_major == 'V8.2':
         from pyvmax.VmaxApi_v82 import VmaxApi
+        log.debug('Importing VmaxApi_v82')
     elif version_major == 'V8.0':
         from pyvmax.VmaxApi_v80 import VmaxApi
+        log.debug('Importing VmaxApi_v80')
     else:
         from pyvmax.VmaxApi_v82 import VmaxApi
-        print('Unsupported VMAX API Version', univmax_version)
-        print('Using latest VMAX API Version')
+        log.debug('Importing VmaxApi_v82')
+        log.warn('Unsupported VMAX API Version', univmax_version)
+        log.warn('Using latest VMAX API Version')
 
     return VmaxApi(rest_client, url)
 
