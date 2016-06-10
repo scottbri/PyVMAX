121053215fa74d0f8649fe2d9178fa2beb6d56b4
Scott Brightwell <scott@brightwell.org>
2016-06-10 10:21:31 -0400

    parsed version string more robustly

--- pyvmax/__init__.py
+++ pyvmax/__init__.py
@@ -1,24 +1,27 @@
 __version__ = "0.8.2"
 
-from .Restful import Restful
+from pyvmax.Restful import Restful
 
 def connect(url, username, password):
 
     rest_client = Restful(url, username, password)
     target_uri = "%s/univmax/restapi/system/version" % (url)
     version_response = rest_client.get(target_uri)
-    univmax_version = None
     if 'version' in version_response:
-        univmax_version = version_response.get('version')
+        univmax_version = version_response.get('version', '')
 
 # import the correct API module for the version of Unisphere discovered
-    if univmax_version == 'V8.2.0.5':
-        from .VmaxApi_v82 import VmaxApi
-    elif univmax_version == 'V8.0.1.7':
-        from .VmaxApi_v80 import VmaxApi
+    version_major, version_minor = ""
+    if '.' in univmax_version:
+        _version_parts = univmax_version.split('.')
+        version_major, version_minor = '.'.join(_version_parts[:n]), '.'.join(_version_parts[n:])
+    if version_major == 'V8.2':
+        from pyvmax.VmaxApi_v82 import VmaxApi
+    elif version_major == 'V8.0':
+        from pyvmax.VmaxApi_v80 import VmaxApi
     else:
-        from .VmaxApi_v82 import VmaxApi
-        print('Unsupported VMAX API Version {}'.format(univmax_version))
+        from pyvmax.VmaxApi_v82 import VmaxApi
+        print('Unsupported VMAX API Version', univmax_version)
         print('Using latest VMAX API Version')
 
     return VmaxApi(rest_client, url)
